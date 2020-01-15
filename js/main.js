93de2a11ab79af74c4f7bf1ed11d45c8a72bdcfa
Graham <g_vickers@fanshaweonline.ca>
2020-01-15 09:44:26 -0500

    nav is almost done

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+
+var vm = new Vue({
+  el: "#app",
+
+  data: {
+
+    // mock up the user - this well eventually come from the database UMS (user management system)
+    user: {  
+      isAdmin: false, 
+      avatar: null,
+      isLoggedIn: true
+    },
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+    setUserPrefs(){
+      // this is the preferences control, hit the api when ready (or use a component)
+      console.log('set the user prefs here!')
+    },
+
+    userLogin(){ 
+      console.log('user login in here')
+    }
+  }
+});
